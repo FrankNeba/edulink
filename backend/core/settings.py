711fe2600070a34cbcfbfe3cba0f38b79411554f
FrankNeba <ngwafranklin29@gmail.com>
2026-03-06 08:36:13 +0100

    fix: remove daphne from INSTALLED_APPS, fix railway.toml start command

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',') + [
 # Application definition
 
 INSTALLED_APPS = [
-    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
